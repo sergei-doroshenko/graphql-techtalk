c0863a274f76b9d61ddcc57f92020de231f5629b
sergei-doroshenko <6823298@gmail.com>
2018-05-21 21:34:26 +0300

    3-Schema and Resolver

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # graphql-techtalk
 GraphQL Java Techtalk sample
 http://localhost:8080/h2-console/login.do
+JDBC URL: jdbc:h2:mem:testdb
--- src/main/java/org/sdoroshenko/GraphQLApplication.java
+++ src/main/java/org/sdoroshenko/GraphQLApplication.java
@@ -1,10 +1,28 @@
 package org.sdoroshenko;
 
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import org.sdoroshenko.model.Car;
+import org.sdoroshenko.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GraphQLApplication {
+
+    @Bean
+    public GraphQLQueryResolver query() {
+        return new GraphQLQueryResolver() {
+            @Autowired
+            CarRepository carRepository;
+
+            public Iterable<Car> findAllCars() {
+                return carRepository.findAll();
+            }
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(GraphQLApplication.class, args);
     }
